391fe53727b53413d47a107417287339acfb912d
rohith <rohithdpr016@gmail.com>
2024-10-25 13:23:27 +0530

    code updated

--- app/Interfaces/PaymentGatewayInterface.php
+++ app/Interfaces/PaymentGatewayInterface.php
@@ -4,6 +4,19 @@ namespace App\Interfaces;
 
 interface PaymentGatewayInterface
 {
+    /**
+     * pay function
+     *
+     * @param float $amount
+     * @return boolean
+     */
     public function pay(float $amount): bool;
+    /**
+     * refund function
+     *
+     * @param integer $transactionId
+     * @param float $amount
+     * @return boolean
+     */
     public function refund(int $transactionId, float $amount): bool;
 }
